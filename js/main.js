c99d466f7e191a91452505380c0b69e06ef73411
Dean gaggero <deangaggero@gmail.com>
2018-04-03 22:19:29 -0700

    week10exercise

--- js/main.js
+++ js/main.js
@@ -1,31 +1,70 @@
-// var testOne = "What kind of variable is this?"; // string
-// var testThree = ["What", "kind", "of", "variable", "am", "I?"]; // object
-// var testFour = false; // boolean
-// var testFive = [1, 2, 5]; //object
-// var testSeven = "What am I?" //string
-// var testEight = true; //boolean
-// var testNine = ["What kind of variable is this?", 2, 5, 19]; // object
-// var string1 = "Hello ";
-// var string2 = "my name is ";
-// var string3 = "your name here";
-// var combinedStrings = string1 + string2 + string3;
-// var num1 = 8;
-// var num2 = 52;
-// var num3 = 60;
-// var combinedNumbers = num1 + num2 + num3;
-// var mixedNum1 = 10;
-// var mixedString1 = "ten";
-// var mixedNum2 = "10";
-// var mixedConcatenation = mixedNum1 + mixedNum2 + mixedString1;
-// var num4 = 5;
-// var num5 = 0;
-// if (num4 > 1) {
-// console.log("Num4 is greater than 1");
-//} else {
-// console.log("Num4 is NOT greater than 1");
-// var num4 = 5;
-// var num5 = 0;
-// if (num4 < num5 || num4 === num5 && true) {
-// console.log("If statement ran!");
-// } else {
-// console.log("Else statement ran");
+let ferrari = {
+  make: 'ferrari',
+  imgPath: 'img/ferrari.jpg',
+  year: 2017,
+  price: 200000,
+  importFee() {
+  return this.price * .1;
+  }
+}
+
+let bugatti = {
+  make: 'bugatti',
+  imgPath: 'img/bugatti.jpg',
+  year: 2018,
+  price: 100000,
+  importFee(){
+  return this.price * .1;
+  }
+}
+
+let mustang = {
+  make: 'mustang',
+  imgPath: 'img/orangemustang.jpg',
+  year: 2016,
+  price: 50000,
+  importFee() {
+  return this.price * .1;
+  }
+}
+
+console.log(ferrari, bugatti, mustang);
+
+function Car (make, imgPath, year, price) {
+this.make = make;
+this.imgPath = imgPath
+this.year = year;
+this.price = price;
+this.importFee = function() {
+  return this.price * 0.1;
+  }
+}
+const carArray = [
+ferrari = new Car('ferrari', 'img/ferrari.jpg', 2017,200000),
+bugatti = new Car('bugatti', 'img/bugatti.jpg', 2018, 100000),
+mustang = new Car('mustang', 'img/orangemustang.jpg', 2016, 50000),
+]
+
+for (let i=0; i <carArray.length; i++) {
+  let newDiv = document.createElement('DIV')
+  let newMake = document.createElement('H1')
+  let newImg =document.createElement('IMG')
+  let newPrice = document.createElement('P')
+  let newYear = document.createElement('P')
+  console.log(newDiv, newMake, newImg, newPrice, newYear)
+  const newMakeText = document.createTextNode(carArray[i].make)
+  const newPriceText = document.createTextNode(carArray[i].price)
+  const newYearText = document.createTextNode(carArray[i].year)
+  newMake.appendChild(newMakeText)
+  newYear.appendChild(newYearText)
+  newPrice.appendChild(newPriceText)
+  newImg.src = carArray[i].imgPath
+  newImg.alt = carArray[i].make
+  newDiv.appendChild(newMake)
+  newDiv.appendChild(newImg)
+  newDiv.appendChild(newPrice)
+  newDiv.appendChild(newYear)
+  document.body.appendChild(newDiv)
+
+
+}
